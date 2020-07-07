5a38ecab3717b200629596e7c13813a482112c21
Alex <sturmmanngr@gmail.com>
2020-07-07 16:41:56 +0700

    popup

--- src/js/prescript.js
+++ src/js/prescript.js
@@ -4,16 +4,18 @@ $(document).ready(function(){
    	loop: true,
   	responsiveClass:true,
   	responsiveRefreshRate: 15,
-    autoWidth:true,
+
     responsive : {
       0:{
             items:1,
-            nav:true
+            dots:false,
         },
-      1000:{
-            items:3,
-            nav:true,
-        }
+      780:{
+            items:2,
+        },
+        1500:{
+              items:3,
+          }
     },
    });
    var audio = $("#sound")[0];
@@ -23,4 +25,22 @@ $(document).ready(function(){
     $("#pause").click(function() {
       audio.pause();
     });
+    $(".img").click(function(){
+    $("#modal-callback").addClass("opened")
+    $(".modal-overlay").addClass("opened")
+    $("body").addClass("hidden")
+    $(this).clone().appendTo("#modal-callback");
+  });
+  $("#close-button-callback").click(function(){
+    $(this).next().remove()
+    $("#modal-callback").removeClass("opened")
+    $(".modal-overlay").removeClass("opened")
+    $("body").removeClass("hidden")
+  });
+  $(".modal-overlay").click(function(){
+    $("#close-button-callback").next().remove()
+    $(this).removeClass("opened")
+    $("body").removeClass("hidden")
+    $("#modal-callback").removeClass("opened")
+  });
 });
