e81ee84cf4d7efee2555ffaa647eab438ae5e788
Alex <sturmmanngr@gmail.com>
2020-07-10 13:13:24 +0700

    new preloader

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,8 +1,3 @@
-document.body.onload = function(){
-  setTimeout(function(){
-    var preloader = document.getElementById('page-preloader');
-    if (!preloader.classList.contains('done')) {
-      preloader.classList.add('done');
-    }
-  }, 1000);
-}
+$(window).on('load', function () {
+      $('.preloader').fadeOut(1500);
+    });
